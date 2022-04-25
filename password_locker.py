d96991f3582028b3e28e4485e7cbacdf5e707ccd
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-25 13:25:00 +0300

    create function to dispaly credentials created by the user

--- password_locker.py
+++ password_locker.py
@@ -33,15 +33,15 @@ def create_credential(user_name,site_name,account_name,password):
 	return new_credential
 
 def save_credential(credential):
-	'''
-	Function to save a newly created credential
-	'''
+	
+	#Function to save a newly created credential
+	
 	Credential.save_credentials(credential)
 
 def display_credentials(user_name):
-	'''
-	Function to display credentials saved by a user
-	'''
+	
+	# Function to display credentials saved by a user
+	
 	return Credential.display_credentials(user_name)
 	
 def copy_credential(site_name):
