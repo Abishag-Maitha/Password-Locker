7c310e7b9391250d41f571062b1a9cfafee19824
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-25 12:36:13 +0300

    Verify user before creating credentials, creat new social site credentials

--- password_locker.py
+++ password_locker.py
@@ -14,22 +14,21 @@ def save_user(user):
 
 def verify_user(first_name,password):
 	
-	# Function that verifies the existance of the user before creating credentials
+	# Function that verifies the existence of the user before creating credentials
 	
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
 def generate_password():
-	'''
-	Function to generate a password automatically
-	'''
+	
+	# Function to generate a password automatically
 	gen_pass = Credential.generate_password()
 	return gen_pass
 
 def create_credential(user_name,site_name,account_name,password):
-	'''
-	Function to create a new credential
-	'''
+	
+	# Function to create a social site new credentials
+	
 	new_credential=Credential(user_name,site_name,account_name,password)
 	return new_credential
 
