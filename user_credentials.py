0ba0d3b57eec0807ca0b5767e0cd3ab1450f09d0
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-25 22:38:52 +0300

    declare global variables and user class

--- user_credentials.py
+++ user_credentials.py
@@ -5,26 +5,24 @@ import string
 # Global Variables
 global users_list 
 class User:
-	'''
-	Class to create user accounts and save their information
-	'''
+	
+	# Class to create user accounts and save their information
+	
 	# Class Variables
 	# global users_list
 	users_list = []
+	
+	# Method to define the properties for each user object will hold
 	def __init__(self,first_name,last_name,password):
-		'''
-		Method to define the properties for each user object will hold.
-		'''
-
+		
 		# instance variables
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
 
 	def save_user(self):
-		'''
-		Function to save a newly created user instance
-		'''
+		
+		# Function to save a newly created user instance
 		User.users_list.append(self)
 		
 class Credential:
