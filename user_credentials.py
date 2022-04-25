ccb1e9e5809127e200c20110abd11f99227711a5
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-25 22:49:27 +0300

    ceate a method that checks if the name and password entered match entries in the users list

--- user_credentials.py
+++ user_credentials.py
@@ -44,9 +44,9 @@ class Credential:
 		return current_user
 
 	def __init__(self,user_name,site_name,account_name,password):
-		'''
-		Method to define the properties for each user object will hold.
-		'''
+		
+		#Method to define the properties for each user object will hold.
+		
 
 		# instance variables
 		self.user_name = user_name
